6dd09f46f99fa4f5905ebc41879c975621f7b0c5
Hayri Cicek <hello@hayricicek.se>
2016-07-20 16:51:23 +0200

    ordering of modules and contents

--- courses/views.py
+++ courses/views.py
@@ -6,11 +6,31 @@ from django.views.generic.base import TemplateResponseMixin, View
 from django.forms.models import modelform_factory
 from django.apps import apps
 
-from braces.views import LoginRequiredMixin, PermissionRequiredMixin
+from braces.views import LoginRequiredMixin, PermissionRequiredMixin, CsrfExemptMixin, JsonRequestResponseMixin
 
 from .models import Course, Module, Content
 from .forms import ModuleFormSet
 
+
+class ModuleOrderView(CsrfExemptMixin,
+                        JsonRequestResponseMixin,
+                        View):
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Module.objects.filter(id=id,
+                    course__owner=request.user).update(order=order)
+        return render.render_json_response({'saved': 'OK'})
+
+
+class ContentOrderView(CsrfExemptMixin,
+                        JsonRequestResponseMixin,
+                        View):
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Content.objects.filter(id=id,
+                    module__course__owner=request.user).update(order=order)
+        return self.render_json_response({'saved': 'OK'})
+        
 
 class ContentCreateUpdateView(TemplateResponseMixin, View):
     module = None
@@ -85,7 +105,7 @@ class ModuleContentListView(TemplateResponseMixin, View):
                                     course__owner=request.user)
 
         return self.render_to_response({'module': module})
-        
+
 
 class CourseModuleUpdateView(TemplateResponseMixin, View):
     template_name = 'courses/manage/module/formset.html'
